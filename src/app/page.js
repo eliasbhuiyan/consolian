8455872cf79548a04a3df4ff338efc03bace3749
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2024-10-29 17:47:30 +0600

    skills

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Banner from "./components/Home/Banner";
 import Services from "./components/Home/Services";
 import { BackgroundBeams } from "./components/Home/BackgroundBeams";
