4ae7771851ced0158e73fce9010674f9137229c3
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2024-11-02 17:14:38 +0600

    scroll

--- src/app/components/Navbar/SideNav.js
+++ src/app/components/Navbar/SideNav.js
@@ -6,17 +6,28 @@ import {
     IconTerminal2,
 } from "@tabler/icons-react";
 import { PiHandGrabbingBold } from "react-icons/pi";
-import { Link } from "react-scroll";
-
+import { useLenis } from '@studio-freight/react-lenis';
+import Link from "next/link";
 
 export function SideNav() {
+    const lenis = useLenis();
+    // const handleScroll = (id) => {
+    //     const section = document.getElementById(id);
+    //     if (section) {
+    //         lenis.scrollTo(section, {
+    //             offset: 0,
+    //             duration: 1.5,  // Adjust scroll duration
+    //             easing: (t) => t * (2 - t),  // Optional easing function
+    //         });
+    //     }
+    // };
 
     return (
         (<div className="w-20 max-w-20 h-fit bg-gradient-to-b from-white to-transparent border border-opacity-10 shadow-[0_25px_25px_rgba(0,0,0,0.05)] py-6 rounded-xl flex items-center justify-center fixed right-2 top-1/2 -translate-y-1/2 z-50 transition-all">
             <ul className="flex flex-col gap-6 items-center pr-1">
                 <li>
                     <Link
-                        to="/"
+                        href="/"
                         className="text-gray-500 w-10 h-10 rounded-full bg-slate-200 flex items-center justify-center hover:w-20 hover:h-20 hover:mr-6 transition-all relative group"
                     >
                         <IconHome className="h-5 w-5" />
@@ -24,17 +35,17 @@ export function SideNav() {
                     </Link>
                 </li>
                 <li>
-                    <Link to="services"
-                        // href="#services"
+                    <p
+                        // onClick={() => handleScroll('services')}
                         className="text-gray-500 w-10 h-10 rounded-full bg-slate-200 flex items-center justify-center hover:w-20 hover:h-20 hover:mr-6 transition-all relative group"
                     >
                         <IconHeartHandshake className="h-5 w-5" />
                         <span className="absolute -left-16 top-1/2 -translate-y-1/2 px-2 py-1 bg-slate-200 border border-slate-300 rounded text-primary text-xs invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-all">Services</span>
-                    </Link>
+                    </p>
                 </li>
                 <li>
                     <Link
-                        to="skills"
+                        href="/"
                         className="text-gray-500 w-10 h-10 rounded-full bg-slate-200 flex items-center justify-center hover:w-20 hover:h-20 hover:mr-6 transition-all relative group"
                     >
                         <PiHandGrabbingBold className="h-5 w-5" />
@@ -43,7 +54,7 @@ export function SideNav() {
                 </li>
                 <li>
                     <Link
-                        to="portfolio"
+                        href="/"
                         className="text-gray-500 w-10 h-10 rounded-full bg-slate-200 flex items-center justify-center hover:w-20 hover:h-20 hover:mr-6 transition-all relative group"
                     >
                         <IconTerminal2 className="h-5 w-5" />
@@ -52,7 +63,7 @@ export function SideNav() {
                 </li>
                 <li>
                     <Link
-                        to="about"
+                        href="/"
                         className="text-gray-500 w-10 h-10 rounded-full bg-slate-200 flex items-center justify-center hover:w-20 hover:h-20 hover:mr-6 transition-all relative group"
                     >
                         <IconLayout className="h-5 w-5" />
@@ -61,7 +72,7 @@ export function SideNav() {
                 </li>
                 <li>
                     <Link
-                        to="faq"
+                        href="/"
                         className="text-gray-500 w-10 h-10 rounded-full bg-slate-200 flex items-center justify-center hover:w-20 hover:h-20 hover:mr-6 transition-all relative group"
                     >
                         <IconLayout className="h-5 w-5" />
