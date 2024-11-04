090a7a2a8ac3338c2ea4d59241bdd1626fccb6fa
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2024-11-04 19:13:30 +0600

    mac

--- src/app/components/Home/der.js
+++ src/app/components/Home/der.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
+import { cn } from "@/lib/utils";
 import {
     IconBrightnessDown,
     IconBrightnessUp,
@@ -23,7 +24,6 @@ import { IconCommand } from "@tabler/icons-react";
 import { IconCaretLeftFilled } from "@tabler/icons-react";
 import { IconCaretDownFilled } from "@tabler/icons-react";
 import Image from "next/image";
-import { cn } from "@/app/lib/utils";
 
 export const MacbookScroll = ({
     src,
@@ -55,83 +55,19 @@ export const MacbookScroll = ({
     return (
         (<div
             ref={ref}
-            className="min-h-[200vh] relative bg-slate-950 w-full h-4/5 overflow-hidden flex flex-col items-center py-0 md:py-80 justify-start flex-shrink-0 [perspective:800px] transform">
-            <div
-                className="absolute top-20 flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0 ">
-                <motion.div
-                    initial={{ opacity: 0.5, width: "15rem" }}
-                    whileInView={{ opacity: 1, width: "30rem" }}
-                    transition={{
-                        delay: 0.3,
-                        duration: 0.8,
-                        ease: "easeInOut",
-                    }}
-                    style={{
-                        backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
-                    }}
-                    className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]">
-                    <div
-                        className="absolute  w-[100%] left-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-                    <div
-                        className="absolute  w-40 h-[100%] left-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
-                </motion.div>
-                <motion.div
-                    initial={{ opacity: 0.5, width: "15rem" }}
-                    whileInView={{ opacity: 1, width: "30rem" }}
-                    transition={{
-                        delay: 0.3,
-                        duration: 0.8,
-                        ease: "easeInOut",
-                    }}
-                    style={{
-                        backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
-                    }}
-                    className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-cyan-500 text-white [--conic-position:from_290deg_at_center_top]">
-                    <div
-                        className="absolute  w-40 h-[100%] right-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
-                    <div
-                        className="absolute  w-[100%] right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-                </motion.div>
-                <div
-                    className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-slate-950 blur-2xl"></div>
-                <div
-                    className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
-                <div
-                    className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-cyan-500 opacity-50 blur-3xl"></div>
-                <motion.div
-                    initial={{ width: "8rem" }}
-                    whileInView={{ width: "16rem" }}
-                    transition={{
-                        delay: 0.3,
-                        duration: 0.8,
-                        ease: "easeInOut",
-                    }}
-                    className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-cyan-400 blur-2xl"></motion.div>
-                <motion.div
-                    initial={{ width: "15rem" }}
-                    whileInView={{ width: "30rem" }}
-                    transition={{
-                        delay: 0.3,
-                        duration: 0.8,
-                        ease: "easeInOut",
-                    }}
-                    className="absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-cyan-400 "></motion.div>
-
-                <div
-                    className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-slate-950 "></div>
-                <motion.h2
-                    initial={{ opacity: 0.5, y: 100 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{
-                        delay: 0.3,
-                        duration: 0.8,
-                        ease: "easeInOut",
-                    }}
-                    className="mt-8 bg-gradient-to-br from-white to-slate-200 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
-                    Build Websites the right way
-                </motion.h2>
-            </div>
-            {/* <div className="containe mt-40 px-5"> */}
+            className="min-h-[200vh] flex flex-col items-center py-0 md:py-80 justify-start flex-shrink-0 [perspective:800px] transform md:scale-100  scale-[0.35] sm:scale-50">
+            <motion.h2
+                style={{
+                    translateY: textTransform,
+                    opacity: textOpacity,
+                }}
+                className="dark:text-white text-neutral-800 text-3xl font-bold mb-20 text-center">
+                {title || (
+                    <span>
+                        This Macbook is built with Tailwindcss. <br /> No kidding.
+                    </span>
+                )}
+            </motion.h2>
             {/* Lid */}
             <Lid
                 src={src}
@@ -141,7 +77,7 @@ export const MacbookScroll = ({
                 translate={translate} />
             {/* Base area */}
             <div
-                className="h-[22rem] w-full max-w-[32rem] bg-gray-200 dark:bg-[#272729] rounded-2xl overflow-hidden relative -z-10">
+                className="h-[22rem] w-[32rem] bg-gray-200 dark:bg-[#272729] rounded-2xl overflow-hidden relative -z-10">
                 {/* above keyboard bar */}
                 <div className="h-10 w-full relative">
                     <div className="absolute inset-x-0 mx-auto w-[80%] h-4 bg-[#050505]" />
@@ -151,7 +87,7 @@ export const MacbookScroll = ({
                         <SpeakerGrid />
                     </div>
                     <div className="mx-auto w-[80%] h-full">
-                        {/* <Keypad /> */}
+                        <Keypad />
                     </div>
                     <div className="mx-auto w-[10%] overflow-hidden  h-full">
                         <SpeakerGrid />
@@ -160,11 +96,16 @@ export const MacbookScroll = ({
                 <Trackpad />
                 <div
                     className="h-2 w-20 mx-auto inset-x-0 absolute bottom-0 bg-gradient-to-t from-[#272729] to-[#050505] rounded-tr-3xl rounded-tl-3xl" />
+                {showGradient && (
+                    <div
+                        className="h-40 w-full absolute bottom-0 inset-x-0 bg-gradient-to-t dark:from-black from-white via-white dark:via-black to-transparent z-50"></div>
+                )}
+                {badge && <div className="absolute bottom-4 left-4">{badge}</div>}
             </div>
-            {/* </div> */}
         </div>)
     );
 };
+
 export const Lid = ({
     scaleX,
     scaleY,
@@ -180,14 +121,14 @@ export const Lid = ({
                     transformOrigin: "bottom",
                     transformStyle: "preserve-3d",
                 }}
-                className="h-[12rem] w-full max-w-[32rem] bg-[#010101] rounded-2xl p-2 relative">
+                className="h-[12rem] w-[32rem] bg-[#010101] rounded-2xl p-2 relative">
                 <div
                     style={{
                         boxShadow: "0px 2px 0px 2px var(--neutral-900) inset",
                     }}
                     className="absolute inset-0 bg-[#010101] rounded-lg flex items-center justify-center">
                     <span className="text-white">
-                        Consolian
+                        <AceternityLogo />
                     </span>
                 </div>
             </div>
@@ -200,11 +141,11 @@ export const Lid = ({
                     transformStyle: "preserve-3d",
                     transformOrigin: "top",
                 }}
-                className="h-96 w-full max-w-[32rem] absolute inset-0 bg-[#010101] rounded-2xl p-2">
+                className="h-96 w-[32rem] absolute inset-0 bg-[#010101] rounded-2xl p-2">
                 <div className="absolute inset-0 bg-[#272729] rounded-lg" />
                 <Image
-                    src="/macbookScreen.png"
-                    alt="consolian"
+                    src={src}
+                    alt="aceternity logo"
                     fill
                     className="object-cover object-left-top absolute rounded-lg inset-0 h-full w-full" />
             </motion.div>
@@ -645,6 +586,25 @@ export const OptionKey = ({
                 width="32"
                 height="32"
                 stroke="none" />
+        </svg>)
+    );
+};
+
+const AceternityLogo = () => {
+    return (
+        (<svg
+            width="66"
+            height="65"
+            viewBox="0 0 66 65"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-3 w-3 text-white">
+            <path
+                d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
+                stroke="currentColor"
+                strokeWidth="15"
+                strokeMiterlimit="3.86874"
+                strokeLinecap="round" />
         </svg>)
     );
 };
