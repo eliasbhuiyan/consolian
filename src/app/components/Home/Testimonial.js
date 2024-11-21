dec069abbd54f8046cc376de3f4c8a9f052fc626
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2024-11-21 13:11:05 +0600

    testimonial

--- src/app/components/Home/Testimonial.js
+++ src/app/components/Home/Testimonial.js
@@ -13,17 +13,19 @@ const Testimonial = () => {
                         type: "loop",
                         gap: "30px",
                         drag: "free",
-                        pagination: true,
+                        pagination: false,
                         focus: 'center',
                         pauseOnHover: false,
                         arrows: 'false',
                         breakpoints: {
-                            767: {
+                            639: {
                                 perPage: 1,
+                                gap: "10px",
                             },
                             1279: {
                                 perPage: 2,
-                            },
+                                gap: "10px",
+                            }
                         },
                         perPage: 3,
                     }} extensions={{ AutoScroll }}>
@@ -33,54 +35,16 @@ const Testimonial = () => {
                                 <Image src="" width={200} height={200} alt='profile' />
                             </div>
                             <div className="content">
-                                <h3 className='text-xl text-primary font-bold font-primary'>Elias Bhuiyan</h3>
-                                <p>SEO Expert</p>
-                                <ul className='flex gap-1 justify-center text-xl text-orange-600'>
+                                <h3 className='text-sm md:text-xl text-primary font-bold font-primary'>Elias Bhuiyan</h3>
+                                <p className='text-sm md:text-base'>SEO Expert</p>
+                                <ul className='flex gap-1 justify-center text-sm md:text-base mb-2 text-orange-600'>
                                     <li><FaStar /></li>
                                     <li><FaStar /></li>
                                     <li><FaStar /></li>
                                     <li><FaStar /></li>
                                     <li><FaStar /></li>
                                 </ul>
-                                <p>GREAT work. Very fast and beautiful design. He got it right the first time which really made me happy. I&apos;m very impressed with the quality and caring Elias put into creating a landing page for my company. Thank you. I&apos;ll highly recommend you.</p>
-                            </div>
-                        </div>
-                    </SplideSlide>
-                    <SplideSlide>
-                        <div className="review">
-                            <div className="icon overflow-hidden">
-                                <Image src="" width={200} height={200} alt='profile' />
-                            </div>
-                            <div className="content">
-                                <h3 className='text-xl text-primary font-bold font-primary'>Elias Bhuiyan</h3>
-                                <p>Mern Stack Developer</p>
-                                <ul className='flex gap-1 justify-center text-xl text-orange-600'>
-                                    <li><FaStar /></li>
-                                    <li><FaStar /></li>
-                                    <li><FaStar /></li>
-                                    <li><FaStar /></li>
-                                    <li><FaStar /></li>
-                                </ul>
-                                <p>Elias&apos;s agency, led by the talented MERN Stack Developer Elias, delivers outstanding results. Their expertise in MongoDB, Express.js, React.js, and Node.js is evident in the high-quality web and mobile applications they produce.</p>
-                            </div>
-                        </div>
-                    </SplideSlide>
-                    <SplideSlide>
-                        <div className="review">
-                            <div className="icon overflow-hidden">
-                                <Image src="" width={200} height={200} alt='profile' />
-                            </div>
-                            <div className="content">
-                                <h3 className='text-xl text-primary font-bold font-primary'>Elias Bhuiyan</h3>
-                                <p>Mern Stack Developer</p>
-                                <ul className='flex gap-1 justify-center text-xl text-orange-600'>
-                                    <li><FaStar /></li>
-                                    <li><FaStar /></li>
-                                    <li><FaStar /></li>
-                                    <li><FaStar /></li>
-                                    <li><FaStar /></li>
-                                </ul>
-                                <p>Elias is very committed and fast communicative. He delivered the project precisely as required without many revisions. I&apos;m looking to work for more projects with him on other website.</p>
+                                <p className='text-xs md:text-sm'>GREAT work. Very fast and beautiful design. He got it right the first time which really made me happy. I&apos;m very impressed with the quality and caring Elias put into creating a landing page for my company. Thank you. I&apos;ll highly recommend you.</p>
                             </div>
                         </div>
                     </SplideSlide>
