e9aa4b960397adb6c88487d242c44dc09ebd06a2
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2024-11-24 15:54:19 +0600

    re touch

--- src/app/components/Framer/TestimonialWrapper.js
+++ src/app/components/Framer/TestimonialWrapper.js
@@ -100,13 +100,13 @@ export const TestimonialWrapper = ({
                             duration: 0.2,
                             ease: "easeInOut",
                         }}>
-                        <h3 className="text-2xl font-bold dark:text-white text-black">
+                        <h3 className="text-2xl font-bold text-black">
                             {testimonials[active].name}
                         </h3>
-                        <p className="text-sm text-gray-500 dark:text-neutral-500">
+                        <p className="text-sm text-gray-500">
                             {testimonials[active].designation}
                         </p>
-                        <motion.p className="text-lg text-gray-500 mt-8 dark:text-neutral-300">
+                        <motion.p className="text-lg text-gray-500 mt-8">
                             {testimonials[active].quote.split(" ").map((word, index) => (
                                 <motion.span
                                     key={index}
@@ -134,15 +134,15 @@ export const TestimonialWrapper = ({
                     <div className="flex gap-4 pt-12 md:pt-0">
                         <button
                             onClick={handlePrev}
-                            className="h-7 w-7 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button">
+                            className="h-7 w-7 rounded-full bg-gray-100 flex items-center justify-center group/button">
                             <IconArrowLeft
-                                className="h-5 w-5 text-black dark:text-neutral-400 group-hover/button:rotate-12 transition-transform duration-300" />
+                                className="h-5 w-5 text-black group-hover/button:rotate-12 transition-transform duration-300" />
                         </button>
                         <button
                             onClick={handleNext}
-                            className="h-7 w-7 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button">
+                            className="h-7 w-7 rounded-full bg-gray-100 flex items-center justify-center group/button">
                             <IconArrowRight
-                                className="h-5 w-5 text-black dark:text-neutral-400 group-hover/button:-rotate-12 transition-transform duration-300" />
+                                className="h-5 w-5 text-black group-hover/button:-rotate-12 transition-transform duration-300" />
                         </button>
                     </div>
                 </div>
