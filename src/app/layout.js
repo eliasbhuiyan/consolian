9af3b4bbfdb1ddb3c3a6c9e6c0028ac9633cd6d7
Your Name <your.email@example.com>
2025-07-30 17:14:06 +0600

    retouch

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,12 @@
 import "./globals.css";
 import { SideNav } from "./components/Navbar/SideNav";
 
-
 export const metadata = {
   title: "Consolian | Software Agency by Elias Bhuiyan",
-  description: "Experienced MERN Stack Developer specializing in Website Design, Web & App Development, UI/UX, SEO Optimization, and Website Maintenance. Explore my portfolio for innovative digital solutions.",
-  keywords: "MERN Stack Developer, Website Design, Web Development, App Development, UI/UX Design, SEO Optimization, Website Maintenance, Consolian, Elias Bhuiyan",
+  description:
+    "Experienced MERN Stack Developer specializing in Website Design, Web & App Development, UI/UX, SEO Optimization, and Website Maintenance. Explore my portfolio for innovative digital solutions.",
+  keywords:
+    "MERN Stack Developer, Website Design, Web Development, App Development, UI/UX Design, SEO Optimization, Website Maintenance, Consolian, Elias Bhuiyan",
   authors: "Elias Bhuiyan",
   creator: "Elias Bhuiyan",
   publisher: "Consolian",
@@ -20,9 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`antialiased cursor shape`}
-      >
+      <body className={`antialiased cursor shape`}>
         <SideNav />
         {children}
       </body>
